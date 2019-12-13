680359e22fd1a600a12aa44e003a14d0bc070e41
heretodesign <senwelo.works@gmail.com>
2019-12-13 18:38:33 +0800

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import ListUserComponent from "./user/ListUserComponent"
+import AddUserComponent from "./user/AddUserComponent"
+import EditUserComponent from "./user/EditUserComponent"
+import { Router, Route, Switch } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <Router>
+        <Switch>
+            <Route path="/" exact component={ListUserComponent} />
+            <Route path="/users" component={ListUserComponent} />
+            <Route path="/add-user" component={AddUserComponent} />
+            <Route path="/edit-user" component={EditUserComponent} />
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
