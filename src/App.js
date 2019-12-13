f443821c3c83181c8533d95db05cb2d2ae1259c2
heretodesign <senwelo.works@gmail.com>
2019-12-13 19:16:19 +0800

    added the user and components attributes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css'
 import ListUserComponent from "./user/ListUserComponent"
 import AddUserComponent from "./user/AddUserComponent"
 import EditUserComponent from "./user/EditUserComponent"
-import { Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Container from '@material-ui/core/Container'
-
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <Container maxWidth="md">
+      <Navbar />
       <Router>
         <Switch>
             <Route path="/" exact component={ListUserComponent} />
---
+++ src/user/ListUserComponent.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import ApiService from "../service/ApiService";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import CreateIcon from '@material-ui/icons/Create';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Typography from '@material-ui/core/Typography';
+
+export default class ListUserComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [],
+            message: null
+        }
+        this.deleteUser = this.deleteUser.bind(this);
+        this.editUser = this.editUser.bind(this);
+        this.addUser = this.addUser.bind(this);
+        this.reloadUserList = this.reloadUserList.bind(this);
+    }
+
+    componentDidMount() {
+        this.reloadUserList();
+    }
+
+    reloadUserList() {
+        ApiService.fetchUsers()
+            .then((res) => {
+                this.setState({users: res.data.result})
+            });
+    }
+
+    deleteUser(userId) {
+        ApiService.deleteUser(userId)
+           .then(res => {
+               this.setState({message : 'User deleted successfully.'});
+               this.setState({users: this.state.users.filter(user => user.id !== userId)});
+           })
+    }
+
+    editUser(id) {
+        window.localStorage.setItem("userId", id);
+        this.props.history.push('/edit-user');
+    }
+
+    addUser() {
+        window.localStorage.removeItem("userId");
+        this.props.history.push('/add-user');
+    }
+
+    render() {
+        return (
+            <div>
+                <Typography variant="h4" style={style}>User Details</Typography>
+                <Button variant="contained" color="primary" onClick={() => this.addUser()}>
+                    Add User
+                </Button>
+
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell>FirstName</TableCell>
+                            <TableCell align="right">LastName</TableCell>
+                            <TableCell align="right">UserName</TableCell>
+                            <TableCell align="right">Age</TableCell>
+                            <TableCell align="right">Salary</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.users.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell component="th" scope="row">
+                                    {row.id}
+                                </TableCell>
+                                <TableCell align="right">{row.firstName}</TableCell>
+                                <TableCell align="right">{row.lastName}</TableCell>
+                                <TableCell align="right">{row.username}</TableCell>
+                                <TableCell align="right">{row.age}</TableCell>
+                                <TableCell align="right">{row.salary}</TableCell>
+                                <TableCell align="right" onClick={() => this.editUser(row.id)}><CreateIcon /></TableCell>
+                                <TableCell align="right" onClick={() => this.deleteUser(row.id)}><DeleteIcon /></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+        )
+    }
+}
+
+const style = {
+    display: 'flex',
+    justifyContent: 'center'
+}
